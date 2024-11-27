32a822ba1a45e0a3bde68d8d1abd6d7cd53b747f
MH Nazmul <mhnazmul3803@gmail.com>
2024-11-27 16:47:13 +0600

    update

--- Dice_Rolling_App/lib/diceroller.dart
+++ Dice_Rolling_App/lib/diceroller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
   @override
@@ -10,11 +12,10 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var ActiveDiceImage = 'assets/dice3.png';
+  var dice = 3;
   void Dice_roll() {
-    var dice = Random().nextInt(6) + 1;
     setState(() {
-      ActiveDiceImage = 'assets/dice$dice.png';
+      dice = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -24,7 +25,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          ActiveDiceImage,
+          'assets/dice$dice.png',
           width: 100,
         ),
         const SizedBox(
